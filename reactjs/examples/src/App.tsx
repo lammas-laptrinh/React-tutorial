5639e914cc51198113d945282d002c4a5ca560d8
kyu0z <duykhaidevfe@gmail.com>
2023-04-17 20:26:41 +0700

    update code ss2-dashboard

--- reactjs/examples/src/App.tsx
+++ reactjs/examples/src/App.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import Menu from "./components/Menu";
 import FormSignIn from "./components/Form/FormSignIn";
 // import { Sidebarr } from "./components/SideBar";
-// import DashboardMain from "./components/Dashboard/DashboardMain";
-// import ErrorBoundary from "antd/es/alert/ErrorBoundary";
+import DashboardMain from "./components/Dashboard/DashboardMain";
+import ErrorBoundary from "antd/es/alert/ErrorBoundary";
 
 const App: React.FC = () => {
   return (
     // <Sidebarr />;
     // <Menu></Menu>
     // <Form></Form>
-    <FormSignIn />
-    // <ErrorBoundary>
-    //   <DashboardMain />
-    // </ErrorBoundary>
+    // <FormSignIn />
+    <ErrorBoundary>
+      <DashboardMain />
+    </ErrorBoundary>
   );
 };
 
