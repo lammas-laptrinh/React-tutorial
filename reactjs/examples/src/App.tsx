543ff59818f4771c344b0e432c9b0d7be02b9288
Tan Phat <nguyenvantanphat.it@gmail.com>
2023-04-25 01:02:36 +0700

    Triển khai và quản lý global state bằng Context API

--- reactjs/examples/src/App.tsx
+++ reactjs/examples/src/App.tsx
@@ -1,32 +1,25 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import SideBar from "./components/SideBar";
 import ClassComponents from "./views/LifeCycle/ClassComponents";
 import FunctionComponents from "./views/LifeCycle/FunctionComponents";
 import "./App.css";
 import React from "react";
 import { SideBarItemProps } from "./components/SideBar/types";
-import HOC from "./components/HOC";
-import DemoHOC from "./components/HOC/DemoHOC";
+import { UserProvider } from "./components/ContextAPI/UserContext";
+import { UserStatusIndicator } from "./components/ContextAPI/UserStatusIndicator";
+import {LoginButton} from "./components/ContextAPI/LoginButton";
+import {LogoutButton} from "./components/ContextAPI/LogoutButton";
+import DashBoard from "./components/DashBoard";
+import Room from "./components/Room";
 
-const dummyData = [
-  { id: 0, name: "Test1", icon: "" },
-  { id: 1, name: "Test2", icon: "" },
-  { id: 2, name: "Test3", icon: "" },
-  { id: 3, name: "Test4", icon: "" },
-];
 function App() {
-  const [items, setItems] = React.useState<any>([]);
-  useEffect(() => {
-    // read file or call APIs
-
-    setItems(dummyData);
-  }, []);
+ 
   return (
-    <HOC>
-      <DemoHOC />
-    </HOC>
+    <UserProvider>
+      <UserStatusIndicator />
+      <LoginButton />
+      <LogoutButton />
+    </UserProvider>
   );
 }
 
