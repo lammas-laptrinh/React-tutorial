4eb063381ec5658dcf6ebfec76b1070ebd5e6607
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-04-24 19:33:52 +0700

    VJP_Session3_NguyenHuyHoang

--- reactjs/examples/src/App.tsx
+++ reactjs/examples/src/App.tsx
@@ -6,8 +6,18 @@ import ClassComponents from "./views/LifeCycle/ClassComponents";
 import FunctionComponents from "./views/LifeCycle/FunctionComponents";
 import "./App.css";
 
+import NewUser from './components/NewUser'
+import UsersList from './components/UsersList'
+import { UserProvider } from './context/UserContext'
+
+
 function App() {
-  return <FunctionComponents />;
+  return
+   <UserProvider>
+  <h1>App is working</h1>
+  <UsersList />
+  <NewUser />
+</UserProvider>;
 }
 
 export default App;
