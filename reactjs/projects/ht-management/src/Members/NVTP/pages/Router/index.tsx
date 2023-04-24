042d327ce7e241373bd59ef227e3a43d8726cb6a
Tan Phat <nguyenvantanphat.it@gmail.com>
2023-04-25 04:02:27 +0700

    Vjp session4 Room  Nguyen Van Tan Phat

---
+++ reactjs/projects/ht-management/src/Members/NVTP/pages/Router/index.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Signin from "../Pages/Signin";
+import Dashboard from "../Pages/Dashboard";
+import PrivateRoute from "./PrivateRoute";
+import NotFound from "../Pages/Error/NotFound";
+import Room from "../Pages/Room/index";
+import RoomDetail from "../Pages/Room/RoomDetail";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Room />} />
+        <Route path="/rooms/:id" Component={RoomDetail} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dash-board" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
