ea9a9a2a231ae64460af60b221261ad12ad9462a
InoriZ <quocthai.jy9a13@gmail.com>
2023-04-27 16:58:24 +0700

    Delete: Remove Dashboard and Service

--- reactjs/projects/ht-management/src/Members/LPQT/Rooms/routes/index.tsx
+++ reactjs/projects/ht-management/src/Members/LPQT/Rooms/routes/index.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import Main from '../pages/Main';
-import Detail from '../pages/Detail';
+import RoomMain from '../pages';
+import RoomDetail from '../pages/Detail';
 
 export default function Room() {
     return (
         <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path=":roomId" element={<Detail />} />
+            <Route path="/" element={<RoomMain />} />
+            <Route path=":roomId" element={<RoomDetail />} />
         </Routes>
     );
 }
