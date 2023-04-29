6063731b1555714295e55e4ededd100526994fb5
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-04-29 20:34:55 +0700

    VJP-ss5-task-LHMTuan

--- reactjs/projects/ht-management/src/Router/index.tsx
+++ reactjs/projects/ht-management/src/Router/index.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Signin from "../Pages/Signin";
+//import Signin from "../Pages/Signin";
 import Dashboard from "../Pages/Dashboard";
 import PrivateRoute from "./PrivateRoute";
 import NotFound from "../Pages/Error/NotFound";
+import Service from "../Members/LHMT/Service";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Signin />} />
+        {/*   <Route path="/" element={<Signin />} />*/}
+        <Route path="/" element={<Service />} />
         <Route path="/dash-board" element={<PrivateRoute />}>
           <Route path="" element={<Dashboard />} />
         </Route>
