e247e78be544e9a1947b05fe75416d36d9d00bc5
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-05-03 21:41:12 +0700

    VJP_Session6_CheckOut_NguyenHuyHoang

--- reactjs/projects/ht-management/src/App.tsx
+++ reactjs/projects/ht-management/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css'
-import Main from './Members/NHH/pages/Home';
 import CheckOut from '../src/Members/NHH/pages/CheckOut/CheckOut';
 function App() {
   return (
