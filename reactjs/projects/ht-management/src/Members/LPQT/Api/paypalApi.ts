a3e46e2891f023d0fe548ad78fc9abb969706170
InoriZ <quocthai.jy9a13@gmail.com>
2023-05-09 14:29:30 +0700

    fix: the create order doesnt need my random id

--- reactjs/projects/ht-management/src/Members/LPQT/Api/paypalApi.ts
+++ reactjs/projects/ht-management/src/Members/LPQT/Api/paypalApi.ts
@@ -1,33 +1,21 @@
 
 import axiosClient from './axiosClient';
 import { IPayPalOrder } from '../Payment/type';
-import { v4 as uuidv4 } from 'uuid';
-
 
 //get the stored Token
 const paypalToken = localStorage.getItem('paypalToken');
+//custome header for API
+const headers = {
+    'Content-Type': 'application/json',
+    'Prefer': 'return=representation',
+    'Authorization': `Bearer ${paypalToken}`
+}
 
 const paypalApi = {
     createOrder: (data: IPayPalOrder): Promise<any> => {
-        //get the random value for 'PayPal-Request-Id
-        const PRId = uuidv4()
-        //custome header for API
-        const headers = {
-            'Content-Type': 'application/json',
-            'Prefer': 'return=representation',
-            'PayPal-Request-Id': PRId,
-            'Authorization': `Bearer ${paypalToken}`
-        }
-        console.log(PRId)
         return axiosClient.post('/v2/checkout/orders', data, { headers: headers });
     },
     checkoutOderById(id: string): Promise<any> {
-        //custome header for API
-        const headers = {
-            'Content-Type': 'application/json',
-            'Prefer': 'return=representation',
-            'Authorization': `Bearer ${paypalToken}`
-        }
         return axiosClient.get(`v2/checkout/orders/${id}`, { headers: headers });
     },
 };
