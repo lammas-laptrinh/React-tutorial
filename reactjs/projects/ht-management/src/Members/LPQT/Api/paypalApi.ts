400a365376f486a02766b2a41bb0272ee24dd75b
InoriZ <quocthai.jy9a13@gmail.com>
2023-05-09 15:17:37 +0700

    fix: add inline header to Api

--- reactjs/projects/ht-management/src/Members/LPQT/Api/paypalApi.ts
+++ reactjs/projects/ht-management/src/Members/LPQT/Api/paypalApi.ts
@@ -2,20 +2,27 @@
 import axiosClient from './axiosClient';
 import { IPayPalOrder } from '../Payment/type';
 
-//get the stored Token
-const paypalToken = localStorage.getItem('paypalToken');
-//custome header for API
-const headers = {
-    'Content-Type': 'application/json',
-    'Prefer': 'return=representation',
-    'Authorization': `Bearer ${paypalToken}`
-}
-
 const paypalApi = {
     createOrder: (data: IPayPalOrder): Promise<any> => {
+        //get the stored Token
+        const paypalToken = localStorage.getItem('paypalToken');
+        //custome header for API
+        const headers = {
+            'Content-Type': 'application/json',
+            'Prefer': 'return=representation',
+            'Authorization': `Bearer ${paypalToken}`
+        }
         return axiosClient.post('/v2/checkout/orders', data, { headers: headers });
     },
     checkoutOderById(id: string): Promise<any> {
+        //get the stored Token
+        const paypalToken = localStorage.getItem('paypalToken');
+        //custome header for API
+        const headers = {
+            'Content-Type': 'application/json',
+            'Prefer': 'return=representation',
+            'Authorization': `Bearer ${paypalToken}`
+        }
         return axiosClient.get(`v2/checkout/orders/${id}`, { headers: headers });
     },
 };
