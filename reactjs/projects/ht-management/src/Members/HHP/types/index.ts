3801a4bcd66d1073b7ad8671aa421476604fbced
shuriko2302 <senziukagi11333@gmail.com>
2023-05-10 20:42:34 +0700

    session 4

---
+++ reactjs/projects/ht-management/src/Members/HHP/types/index.ts
@@ -0,0 +1,10 @@
+export type Rooms = {
+    id: any;
+    roomName: string;
+    bedAmount: number;
+    checkinDate: string;
+    checkoutDate: string;
+    roomType: string;
+    serviceCount: number;
+    service?: string[];
+};
