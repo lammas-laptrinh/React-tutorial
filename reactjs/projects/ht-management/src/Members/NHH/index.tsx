e99efe37c8b7ac509c29c2109596d4b014122697
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-05-19 14:41:24 +0700

    fix SS5

--- reactjs/projects/ht-management/src/Members/NHH/index.tsx
+++ reactjs/projects/ht-management/src/Members/NHH/index.tsx
@@ -1,10 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './pages/Home';
-=======
-import Main from './pages/main';
-import Detail from './pages/RoomDetail/roomDetail';
->>>>>>> d33436fbe7f5c00a756708e2c4e83e9d17524acb
 export default function Routers() {
     return (
         <Routes>
