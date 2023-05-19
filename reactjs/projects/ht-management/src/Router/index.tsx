53fb37b3067c5c249bd82a4a849ea1c0b9ecbf50
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-05-19 15:07:26 +0700

    fix router

--- reactjs/projects/ht-management/src/Router/index.tsx
+++ reactjs/projects/ht-management/src/Router/index.tsx
@@ -1,30 +1,24 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import NotFound from "../Pages/Error/NotFound";
-import RoomPageTuan from "../Members/LHMTuan/Room";
-import RoomDetailTuan from "../Members/LHMTuan/Room/RoomDetail";
-import ServicePage from "../Members/LHMT/Service";
-// import KingDetail from "@src/Members/MDK/RoomDetail/KingDetail";
-// import StandardDetail from "@src/Members/MDK/RoomDetail/StandardDetail";
-// import DoubleDetail from "@src/Members/MDK/RoomDetail/DoubleDetail";
-// import Dashboard from "@src/Members/MDK/Dashboard";
-//import Signin from "@src/Pages/Signin";
+import Room from "../Members/LHMTuan/Room";
+import RoomDetail from "../Members/LHMTuan/Room/RoomDetail";
+import KingDetail from "@src/Members/MDK/RoomDetail/KingDetail";
+import StandardDetail from "@src/Members/MDK/RoomDetail/StandardDetail";
+import DoubleDetail from "@src/Members/MDK/RoomDetail/DoubleDetail";
+import Dashboard from "@src/Members/MDK/Dashboard";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<RoomPageTuan />} />
-        <Route path=":id" element={<RoomDetailTuan />} />
-        <Route path="/service" element={<ServicePage />} />
-        {/* <Route path="/" element={<Signin />} />
-        <Route path="/dash-board" element={<PrivateRoute />}>
-          <Route path="" element={<Dashboard />} />
-        </Route> */}
-        {/* <Route path="/" element={<Dashboard />} />
+        <Route path="/room-management" element={<Room />} />
+        <Route path="/room-detail" element={<RoomDetail />} />
+        <Route path="" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/standard-detail" element={<StandardDetail />} />
         <Route path="/double-detail" element={<DoubleDetail />} />
-        <Route path="/king-detail" element={<KingDetail />} /> */}
+        <Route path="/king-detail" element={<KingDetail />} />
       </Routes>
     </BrowserRouter>
   );
