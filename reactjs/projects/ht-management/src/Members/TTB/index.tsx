b61b1127b6313b728e9588fff56b6d41e1709729
Thien Bao <truongbao901@gmail.com>
2023-05-26 13:59:59 +0700

    fix leaflet

--- reactjs/projects/ht-management/src/Members/TTB/index.tsx
+++ reactjs/projects/ht-management/src/Members/TTB/index.tsx
@@ -1,46 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L, { LeafletMouseEvent } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
 
-// Define a custom icon for the marker
-const icon = L.icon({
-  iconUrl: 'marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowUrl: 'marker-shadow.png',
-  shadowSize: [41, 41],
-  shadowAnchor: [12, 41],
-});
+import { MapContainer, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import "leaflet-routing-machine";
+import RoutingMachine from "./Routing";
 
 export default function Map() {
-  const mapRef = React.useRef<L.Map>(null);
-  const [position, setPosition] = React.useState<[number, number]>([10.853049657597294, 106.80719688624798]);
-
-  function handleClick(e: LeafletMouseEvent) {
-    setPosition([e.latlng.lat, e.latlng.lng]);
-  }
-
-  function handleMapReady() {
-    if (mapRef.current) {
-      // You now have access to the initialized Leaflet.Map instance via mapRef.current
-    }
-  }
-
-  return (
-    <MapContainer center={position} zoom={13} style={{ height: '100vh' }} whenReady={handleMapReady}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>"
-      />
-      <Marker position={position} icon={icon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
-}
+    return (
+        <MapContainer
+            center={[10.8981541984873, 106.69259211186768]}
+            zoom={13}
+            scrollWheelZoom={true}
+            style={{ width: "100%", height: "calc(100vh - 4rem)" }}
+        >
+            <TileLayer
+                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <RoutingMachine />
+        </MapContainer>
+    );
+};
 
